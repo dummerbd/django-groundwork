7d3b0f9f815bc77a4b9e40fa5f5c7180c29a472d
Ben Dummer <dummerbenjamin@gmail.com>
2015-07-13 11:27:57 -0400

    Added initial implementation of file system watch

--- groundwork/tools.py
+++ groundwork/tools.py
@@ -4,6 +4,7 @@ tools.py - common tasks for installing, configuring, and using external tools.
 import sys
 import os
 import re
+import time
 from os import path
 from tempfile import NamedTemporaryFile
 from contextlib import contextmanager
@@ -11,7 +12,7 @@ from contextlib import contextmanager
 import jsmin
 
 from groundwork.settings import get_setting
-from groundwork.components import get_sass_imports, get_js_files
+from groundwork import components
 
 
 BASE_DIR = path.abspath(path.join(path.dirname(path.abspath(__file__)), '..'))
@@ -60,6 +61,29 @@ class Tool:
         finally:
             os.chdir(old_path)
 
+    def get_sass_include_paths(self):
+        """
+        Get a list of file paths that contain Sass code.
+        """
+        return list(get_setting('sass_include_paths')) + [get_setting('foundation_sass_path')]
+
+    def get_js_files(self):
+        """
+        Get a list of the Js files to be built.
+        """
+        main_js_file = os.path.join(get_setting('foundation_js_path'), 'foundation.js')
+        return [main_js_file] + list(components.get_js_files())
+
+    def makedirs(self, *paths):
+        """
+        Wrapper around `os.makedirs`, fails silently incase directory already
+        exists.
+        """
+        try:
+            for path in paths: os.makedirs(path)
+        except:
+            pass
+
     def run_external_tool(self, command, in_dir=None, redirect_stderr=True):
         """
         Run a shell command.
@@ -106,21 +130,14 @@ class BuildSassTool(Tool):
         self.info('App', app_name)
         self.info('Settings', settings_name)
 
-        output = get_setting('sass_output')
-        min_output = get_setting('sass_min_output')
-        try:
-            os.makedirs(os.path.dirname(output))
-            os.makedirs(os.path.dirname(min_output))
-        except:
-            pass
+        output, min_output = get_setting('sass_output'), get_setting('sass_min_output')
+        self.makedirs(output, min_output)
 
-        include_paths = list(get_setting('sass_include_paths')) + [
-            get_setting('foundation_sass_path')
-        ]
+        include_paths = self.get_sass_include_paths()
         self.info('Paths')
-        [self.info(msg=path) for path in include_paths]
+        for path in include_paths: self.info(msg=path) 
 
-        imports = [settings_name] + list(get_sass_imports()) + [app_name]
+        imports = [settings_name] + list(components.get_sass_imports()) + [app_name]
         includes = ' '.join(['--include-path %s' % path for path in include_paths])
 
         self.info('Output', output)
@@ -149,22 +166,12 @@ class BuildJsTool(Tool):
     def run(self, *args, **kwargs):
         self.info('Js', 'building...')
 
-        js_root = get_setting('foundation_js_path')
-        main_js_file = os.path.join(js_root, 'foundation.js')
-        self.info('Foundation Dir', js_root)
-
-        output = get_setting('js_output')
-        min_output = get_setting('js_min_output')
-        try:
-            os.makedirs(os.path.dirname(output))
-            os.makedirs(os.path.dirname(min_output))
-        except:
-            pass
+        output, min_output = get_setting('js_output'), get_setting('js_min_output')
+        self.makedirs(output, min_output)
 
         self.info('Output', output)
-        js_files = [main_js_file] + get_js_files()
         source = ''
-        for path in js_files:
+        for path in self.get_js_files():
             with open(path, 'r') as src_file:
                 source += src_file.read()
 
@@ -193,6 +200,55 @@ class BuildTool(Tool):
 
 class WatchTool(Tool):
     """
-    Watch for filesystem changes and build when needed.
+    Watch for filesystem changes and build when needed. This tool is optional
+    and requires installing `watchdog`.
     """
-    pass
+    def __init__(self, *args, **kwargs):
+        """
+        Delegate building assets to the Js and Sass tools.
+        """
+        super().__init__(*args, **kwargs)
+        self.js_tool = BuildJsTool(*args, **kwargs)
+        self.sass_tool = BuildSassTool(*args, **kwargs)
+
+    def on_sass_file_event(self, event):
+        """
+        Handle a file event on the Sass paths.
+        """
+        self._on_file_event(event, self.sass_tool)
+
+    def on_js_file_event(self, event):
+        """
+        Handle a file event on the Js paths.
+        """
+        self._on_file_event(event, self.js_tool)
+
+    def _on_file_event(self, event, tool):
+        try:
+            tool.run()
+        except ToolFailureError as e:
+            self.write(e.output)
+
+    def run(self, *args, **kwargs):
+        from watchdog.observers import Observer
+        from watchdog.events import FileSystemEventHandler as Handler
+
+        sass_paths = self.get_sass_include_paths()
+        js_paths = [get_setting('foundation_js_path')]
+
+        sass_handler = type('Handler', (Handler,), {'dispatch': self.on_sass_file_event})()
+        js_handler = type('Handler', (Handler,), {'dispatch': self.on_js_file_event})()
+        observer = Observer()
+
+        for path in sass_paths:
+            observer.schedule(sass_handler, path, recursive=True)
+        for path in js_paths:
+            observer.schedule(js_handler, path, recursive=True)
+
+        self.write('Watching...')
+        observer.start()
+        try:
+            while True: time.sleep(1)
+        except:
+            observer.stop()
+        observer.join()
