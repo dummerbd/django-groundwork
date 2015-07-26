8bd1c53e9f85defe0bc76d7179b66c30cea8ba0c
Ben Dummer <dummerbd@appstate.edu>
2015-07-25 23:11:05 -0400

    Bumped version to 1.0.0-beta for now, and changed license to MIT from BSD

--- setup.py
+++ setup.py
@@ -3,30 +3,44 @@ from setuptools import setup
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
-# allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-groundwork',
-    version='0.2.5',
+
+    version='1.0.0-beta',
+
     packages=['groundwork'],
+
     include_package_data=True,
-    license='BSD License',
+
+    license='MIT License',
+
     description='A simple Django wrapper for Zurb Foundation',
     long_description=README,
+
+    keywords='zurb foundation sass groundwork django',
+
     url='https://github.com/dummerbd/django-groundwork',
+
     author='Benjamin Dummer',
-    author_email='dummerbd@appstate.edu',
+    author_email='dummerbenjamin@gmail.edu',
+
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
         'Operating System :: OS Independent',
-        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Pre-processors'
     ]
 )
